434754625e55789be9a1981d1bb892d8cab4a98f
riveriou <riverriou@yahoo.com.tw>
2023-12-13 01:30:30 +0800

    Create mssql.sh

---
+++ mssql.sh
@@ -0,0 +1,22 @@
+
+#匯入公開存放庫 GPG 金鑰
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
+
+#手動下載並註冊 SQL Server Ubuntu 存放庫
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | tee /etc/apt/sources.list.d/mssql-server-2022.list
+
+#執行下列命令安裝 SQL Server
+apt-get update
+apt-get install -y mssql-server
+
+curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+apt-get install mssql-tools18 unixodbc-dev
+
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+/opt/mssql/bin/mssql-conf setup
+
