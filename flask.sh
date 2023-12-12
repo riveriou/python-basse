b32d2c5aa7908811f1e5b761d9279679674afca3
riveriou <riverriou@yahoo.com.tw>
2023-12-13 00:55:24 +0800

    Create flask.sh

---
+++ flask.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#apt-get -y install python3 python3-pip
+#curl -O https://bootstrap.pypa.io/get-pip.py
+#python3 get-pip.py --user
+pip install Cython flask pymssql SQLAlchemy
+#pip install --no-cache-dir -r requirements.txt
