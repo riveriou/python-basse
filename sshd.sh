63ea01cac3d8c10d3db4f353aaaf797829f46155
riveriou <riverriou@yahoo.com.tw>
2023-12-13 00:38:58 +0800

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /var/run/sshd
 echo 'root:root' | chpasswd
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
   
 # SSH login fix. Otherwise user is kicked off after login
 #sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
