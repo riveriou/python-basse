b1ba33ac1460a2c4cb5b7a662a6ceb8717d9fdcd
riveriou <riverriou@yahoo.com.tw>
2023-12-13 16:00:50 +0800

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -2,5 +2,5 @@
 #apt-get -y install python3 python3-pip
 #curl -O https://bootstrap.pypa.io/get-pip.py
 #python3 get-pip.py --user
-pip install Cython flask pymssql SQLAlchemy
+pip install flask pymssql SQLAlchemy
 #pip install --no-cache-dir -r requirements.txt
